ebfecb1777fbe5087305d8856560b0398408095b
David <davidgonzalezj97@gmail.com>
2023-11-08 13:36:33 +0100

    Exercise 2.6 Creating the handlers to add new names

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,45 @@
 import { useState } from 'react'
 
+const NumberList = ({list}) =>{
+  return (
+    <ul>
+      {list.map(person =><li key={person.name} >{person.name}</li>)}
+    </ul>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
 
+  const handleInputChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const peep = {name : newName}
+    setPersons(persons.concat(peep))
+    setNewName('')
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input />
+          name: <input 
+          value={newName}
+          onChange={handleInputChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <NumberList list={persons}/>
     </div>
   )
 }
