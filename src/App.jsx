3f96b09f1e3aebb4c9f0d8fe2f789e94c108aae9
David <davidgonzalezj97@gmail.com>
2023-11-08 13:57:50 +0100

    Exercise 2.8 Storing numbers too

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,42 @@ import { useState } from 'react'
 const NumberList = ({list}) =>{
   return (
     <ul>
-      {list.map(person =><li key={person.name} >{person.name}</li>)}
+      {list.map(person =><li key={person.name} >{person.name} {person.number}</li>)}
     </ul>
   )
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: '8734792837'  
+  }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-  const handleInputChange = (event) => {
+  const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   const handleSubmit = (event) => {
     event.preventDefault()
     const found = persons.find((person)=>person.name === newName)
     if (found === undefined){
-      const peep = {name : newName}
+      const peep = {name : newName,
+      number : newNumber}
       setPersons(persons.concat(peep))
       setNewName('')
+      setNewNumber('')
     }
     else{
       alert(`${newName} is already in the phonebook`)
       setNewName('')
+      setNewNumber('')
     }
   }
 
@@ -39,12 +49,18 @@ const App = () => {
         <div>
           name: <input 
           value={newName}
-          onChange={handleInputChange}/>
+          onChange={handleNameChange}/>
+        </div>
+        <div>
+          number: <input 
+          value={newNumber}
+          onChange={handleNumberChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
+      
       <h2>Numbers</h2>
       <NumberList list={persons}/>
     </div>
