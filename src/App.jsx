eba03534ba7ee89e4e0581c467aa4a1317077d48
David <davidgonzalezj97@gmail.com>
2023-11-08 15:01:43 +0100

    Exercise 2.9 Adding a search filter

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,15 @@ const NumberList = ({list}) =>{
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '8734792837'  
-  }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -23,6 +26,22 @@ const App = () => {
 
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value)
+  }
+
+  //Auxiliar method to filter the list
+  const checkFilter = (person) =>{
+    const name = person.name
+    const strToCompare = name.substring(0,newFilter.length)
+    return strToCompare.toUpperCase() === newFilter.toUpperCase()
+  }
+
+  const getFilteredList = () => {
+    if (newFilter === '') return persons
+    else return persons.filter(checkFilter)
   }
 
   const handleSubmit = (event) => {
@@ -42,9 +61,17 @@ const App = () => {
     }
   }
 
+  const listToShow = getFilteredList()
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+          filter shown with: <input 
+          value={newFilter}
+          onChange={handleFilterChange}/>
+      </div>
+      <h2>Add a new one</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input 
@@ -62,7 +89,7 @@ const App = () => {
       </form>
       
       <h2>Numbers</h2>
-      <NumberList list={persons}/>
+      <NumberList list={listToShow}/>
     </div>
   )
 }
