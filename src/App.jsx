5562b748ad506a6f8eae570d66f4dedf9205057c
David <davidgonzalezj97@gmail.com>
2023-11-10 14:01:33 +0100

    Exercise 2.11 using useEffect to get the initial data from remote server

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const NumberList = ({list}) =>{
   return (
@@ -39,16 +40,22 @@ const FormForAdding = ({name,number,nameHandler,numberHandler,submitHandler}) =>
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
+
+  //Asks the server for the necesary info (initial numbers)
+  const getListFromServer = () => {
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      console.log('got it!')
+      setPersons(response.data)
+    })
+  }
+
+  useEffect(getListFromServer,[])
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
